bf1024e87d5d8358e785a552abc87aea686ef139
Olivia Tseng <yutingtseng0203@gmail.com>
2024-11-28 20:59:04 -0800

    Create folders by categories

--- NLST/NLST_combine_pid.py
+++ NLST/NLST_combine_pid.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load the CSV files into DataFrames
-file1_path = 'E:/Data/NLST/Radiology CT Images/manifest-NLST_allCT/metadats.csv' 
+file1_path = 'U:/GitHub/AI-ethnicity-med-image/data/NLST_CT_withPID.csv' 
 file2_path = 'E:/Data/NLST/Participant Information.csv' 
 
 # Read the CSV files
@@ -9,10 +9,10 @@ df1 = pd.read_csv(file1_path, dtype=str)
 df2 = pd.read_csv(file2_path, dtype=str)
 
 # Match the patient ID and find their race
-df1 = df1.merge(df2[['pid', 'race']], left_on='Subject ID', right_on='pid', how='left')
+df1 = df1.merge(df2[['pid', 'ethnic']], left_on='Subject ID', right_on='pid', how='left')
 
 # Optionally rename the 'race' column in df1 for clarity
-df1.rename(columns={'race': 'Patient Race'}, inplace=True)
+df1.rename(columns={'ethnic': 'Ethnicity'}, inplace=True)
 
 # Drop the 'pid' column if not needed
 df1.drop(columns=['pid'], inplace=True)
@@ -21,7 +21,7 @@ df1.drop(columns=['pid'], inplace=True)
 df1 = df1.dropna(axis=1, how='all')
 
 # Save the combined DataFrame to a new CSV file
-output_path = 'NLST_file_pid.csv'
+output_path = 'U:/GitHub/AI-ethnicity-med-image/data/NLST_CT_withPID_1.csv'
 df1.to_csv(output_path, index=False)
 
 print("New CSV file with combined race information has been created.")
